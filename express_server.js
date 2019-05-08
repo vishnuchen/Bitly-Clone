f3bd23582ef6aa8365f607b218a86e9547813302
Vishnu Chennuru <vishnuchen@gmail.com>
2019-05-08 00:37:48 +0000

    almost there

--- express_server.js
+++ express_server.js
@@ -4,10 +4,28 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs")
 
+
+function generateRandomString() {
+
+}
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
