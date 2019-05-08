341b89764cc65b58c8f947f2ba6f3af9df5d59a8
Vishnu Chennuru <vishnuchen@gmail.com>
2019-05-08 20:42:41 +0000

    added update url feature

--- express_server.js
+++ express_server.js
@@ -31,13 +31,18 @@ app.post("/urls", (req, res) => {
   console.log(req.body.longURL);  // Log the POST request body to the console
   urlDatabase[shortURL] = req.body.longURL;
   console.log(urlDatabase)
-
   res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   console.log("Deleted links =" +req.params.shortURL+":" +urlDatabase[req.params.shortURL]);
   delete urlDatabase[req.params.shortURL];
+  res.redirect(`/urls`);
+});
+
+app.post("/urls/:id", (req, res) => {
+  var shortURL = req.params.id;
+  urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls`);
 });
 
